8aa47a1623e2da4a1f2d6c59941eb1a123b16283
Pablo José <pablojosemunozaranda@gmail.com>
2025-07-15 17:14:51 +0200

    fixed main.py

--- main.py
+++ main.py
@@ -1,19 +1,24 @@
 import sys
 
-from operators import add, divide, multiply, substract
+from operators import add, divide, multiply, subtract
 
 
 def main():
+    if len(sys.argv) != 4:
+        sys.exit("Usage: python3 main.py <operator> <number1> <number2>")
+
     arguments_list = sys.argv[2:]
 
     operator_functions = {
         "divide": divide,
         "add": add,
-        "substract": substract,
+        "subtract": subtract,
         "multiply": multiply
         }
     
-    if operator_functions not in arguments_list:
+    print(str(sys.argv[1]))
+
+    if sys.argv[1] not in operator_functions:
         sys.exit("No operator specified")
 
 
@@ -21,17 +26,16 @@ def main():
 
     operator_func = operator_functions.get(operator_name)
 
-    if len(sys.argv[2:]) == 2:
-        while True:
-            try:
-                number1 = float(sys.argv[2])
-                number2 = float(sys.argv[3])
-            except (IndexError, ValueError):
-                print("Please provide two numeric arguments after the operator.")
-                pass
-            else: 
-                break
 
+    if len(sys.argv[2:]) != 2:
+        sys.exit("Two numbers must be given")
+
+    try:
+        number1 = float(sys.argv[2])
+        number2 = float(sys.argv[3])
+    except (IndexError, ValueError):
+        sys.exit("Please provide two numeric arguments after the operator.")
+    
     result = operator_func(number1, number2)
 
     print(result)
